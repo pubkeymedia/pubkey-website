b636d04243535d2b41da13e0eb10ca8c63bf63ff
Aryan Jabbari <AryanJabbari@gmail.com>
2022-11-11 12:26:50 -0500

    feat(sitemap): add

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  exclude: ['/try-again'],
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  siteUrl: 'https://pubkey.bar',
+};
