725a023a063787b9c978699db0457722e9ca0f49
Aryan Jabbari <AryanJabbari@gmail.com>
2023-07-08 19:37:27 -0400

    feat(header): add events link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,11 @@ import React, { PropsWithChildren } from 'react';
 
 export const Header = () => {
   return (
-    <header className="font-redaction italic px-2 lg:px-8 max-w-7xl mx-auto tracking-widest w-full">
-      <nav className="flex justify-between text-xl">
+    <header className="font-redaction italic px-2 sm:px-8 max-w-7xl mx-auto tracking-widest w-full">
+      <nav className="flex justify-between md:text-xl">
         <HeaderLink href="/home">Home</HeaderLink>
         <HeaderLink href="/shop">Shop</HeaderLink>
+        <HeaderLink href="/events">Events</HeaderLink>
         <HeaderLink href="/menu">Menu</HeaderLink>
         <HeaderLink href="/contact">Contact</HeaderLink>
       </nav>
@@ -16,16 +17,14 @@ export const Header = () => {
   );
 };
 
-const HeaderLink: React.FC<PropsWithChildren<{ href: string }>> = ({ children, href }) => {
+const HeaderLink: React.FC<PropsWithChildren<{ href: string }>> = (props) => {
   const router = useRouter();
-  const isSelected = router.pathname === href;
+  const isSelected = router.pathname === props.href;
 
   return (
     <Link
       className={clsx('py-4 px-3 md:px-8', isSelected && 'underline underline-offset-4')}
-      href={href}
-    >
-      {children}
-    </Link>
+      {...props}
+    />
   );
 };
