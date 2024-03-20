ada29d3dfd93284f8c81b6a869eb9ffcff6bab94
Aryan Jabbari <AryanJabbari@gmail.com>
2024-03-20 16:19:11 -0400

    Add impact-site-verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* @ts-expect-error */}
+        <meta name="impact-site-verification" value="47925eeb-9cab-4b11-97d9-e900204998a3" />
+      </head>
       <body>{children}</body>
     </html>
   );
