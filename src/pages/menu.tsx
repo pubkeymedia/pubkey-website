6487f3efd529e86632c639aac5aa9e09f33eef71
Daniel Modell <daniel@port8333.io>
2024-04-09 20:27:33 -0400

    Update menu.tsx

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -76,14 +76,6 @@ const Menu = () => {
               </div>
               <div>
                 <FoodName>
-                  Chop Cheese<sup>*</sup> &mdash; $14
-                </FoodName>
-                <FoodDescription>
-                  Wagyu, pepper, onions, American cheese, bodega sauce, hero roll
-                </FoodDescription>
-              </div>
-              <div>
-                <FoodName>
                   Fried Chicken<sup>*</sup> &mdash; $15
                 </FoodName>
                 <FoodDescription>Katsu sauce, slaw, lemon mayo, thick cut toast</FoodDescription>
