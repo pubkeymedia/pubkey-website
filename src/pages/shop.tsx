1074f9ecc463cbdd506ae085322180c5ebbe045d
Daniel Modell <daniel@port8333.io>
2024-04-09 21:52:53 -0400

    Update shop.tsx

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -8,7 +8,7 @@ const ShopPage: NextPage<Props> = ({ products }) => {
   return (
     <div className="flex flex-col gap-y-16 w-full">
       <NextSeo
-        description="The Pubkey store is the place for merch. Who doesn’t love merch? When I walk into my favorite dive bar I except to see merchandise! And when the trains aren’t running, I want to be able to buy my favorite merch online! So I go straight to the Pubkey online store and pick up my favorite merch straight from my couch!. It rules!"
+        description="The PubKey store is the place for merch. Who doesn’t love merch? When I walk into my favorite dive bar I expect to see merchandise! And when the trains aren’t running, I want to be able to buy my favorite merch online! So I go straight to the PubKey online store and pick up my favorite merch straight from my couch! It rules!"
         title="Shop"
       />
       <Image
