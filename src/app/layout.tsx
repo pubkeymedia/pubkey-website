6dce8bc71509399a8fb7bd6d023503862c4f5d44
Daniel Modell <daniel@port8333.io>
2024-06-04 13:10:14 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,10 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Head>
-        {/* Your existing meta tag */}
+      <head>
+        {/* @ts-expect-error */}
         <meta name="impact-site-verification" value="47925eeb-9cab-4b11-97d9-e900204998a3" />
-        {/* Google Tag Manager Script */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZN1D29K1JP"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-ZN1D29K1JP');
-        </script>
-      </Head>
+      </head>
       <body>{children}</body>
     </html>
   );
