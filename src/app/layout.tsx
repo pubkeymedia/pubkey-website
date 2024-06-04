fc1a129e661d123b51c75a1c2bcecf34c94f419b
Daniel Modell <daniel@port8333.io>
2024-06-04 13:08:32 -0400

    + Google tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        {/* @ts-expect-error */}
+      <Head>
+        {/* Your existing meta tag */}
         <meta name="impact-site-verification" value="47925eeb-9cab-4b11-97d9-e900204998a3" />
-        <!-- Google tag (gtag.js) -->
-  <><script async src="https://www.googletagmanager.com/gtag/js?id=G-ZN1D29K1JP"></script><script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments)};}
-    gtag('js', new Date());
-
-    gtag('config', 'G-ZN1D29K1JP');
-  </script></>
-      </head>
+        {/* Google Tag Manager Script */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZN1D29K1JP"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ZN1D29K1JP');
+        </script>
+      </Head>
       <body>{children}</body>
     </html>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,14 +6,6 @@ export default function Document() {
       <Head>
         {/* @ts-expect-error */}
         <meta name="impact-site-verification" value="47925eeb-9cab-4b11-97d9-e900204998a3" />
-        <!-- Google tag (gtag.js) -->
-  <><script async src="https://www.googletagmanager.com/gtag/js?id=G-ZN1D29K1JP"></script><script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments)};}
-    gtag('js', new Date());
-
-    gtag('config', 'G-ZN1D29K1JP');
-  </script></>  
       </Head>
       <body>
         <Main />
