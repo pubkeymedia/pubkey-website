fe3de41fd58c5eb3d3138039ca156eef6dff8c23
Daniel Modell <daniel@port8333.io>
2024-06-04 13:02:09 -0400

    + Google tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         {/* @ts-expect-error */}
         <meta name="impact-site-verification" value="47925eeb-9cab-4b11-97d9-e900204998a3" />
+        <!-- Google tag (gtag.js) -->
+  <><script async src="https://www.googletagmanager.com/gtag/js?id=G-ZN1D29K1JP"></script><script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments)};}
+    gtag('js', new Date());
+
+    gtag('config', 'G-ZN1D29K1JP');
+  </script></>
       </head>
       <body>{children}</body>
     </html>
