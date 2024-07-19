caec948e585a49dd69ad4f2d414847c5e8c46937
Daniel Modell <daniel@port8333.io>
2024-07-19 16:50:26 -0400

    Remove VIP links

--- src/app/rhr/page_bak.tsx
+++ src/app/rhr/page_bak.tsx
@@ -1,34 +1,32 @@
 import React from 'react';
 
-const SideBySideImagesWithPrice: React.FC<{ imageUrl1: string, linkUrl1: string, itemName1: string }> = ({ imageUrl1, linkUrl1, itemName1 }) => {
+const SideBySideImagesWithPrice: React.FC<{ imageUrl1: string, imageUrl2: string, linkUrl1: string, linkUrl2: string, itemName1: string, itemName2: string }> = ({ imageUrl1, imageUrl2, linkUrl1, linkUrl2, itemName1, itemName2 }) => {
   return (
     <div className="side-by-side-container">
       <div className="item-container">
         <a href={linkUrl1} target="_blank" rel="noopener noreferrer">
-          <img src={imageUrl1} alt="Image 1" className="item-image smaller-image" />
+          <img src={imageUrl1} alt="Image 1" className="item-image" />
         </a>
         <div className="item-name">{itemName1}</div>
-        <div className="item-price">
-          <a href={linkUrl1} target="_blank" rel="noopener noreferrer">
-            <div className="button-container">
-              <button style={{
-                fontFamily: 'Arial, sans-serif',
-                fontStyle: 'bold',
-                backgroundColor: '#F66E22',
-                border: 'none',
-                color: 'white',
-                padding: '15px 32px',
-                textAlign: 'center',
-                display: 'inline-block',
-                fontSize: '14px',
-                borderRadius: '5px',
-                margin: '0 auto',
-              }}>
-                <b>$50 STILL AVAILABLE</b>
-              </button>
-            </div>
-          </a>
+        <div className="item-price"><a href={linkUrl1} target="_blank" rel="noopener noreferrer">
+        <div className="button-container">
+            <button style={{
+              fontFamily: 'Arial, sans-serif',
+              fontStyle: 'bold',
+              backgroundColor: '#F66E22',
+              border: 'none',
+              color: 'white',
+              padding: '15px 32px',
+              textAlign: 'center',
+              display: 'inline-block',
+              fontSize: '14px',
+              borderRadius: '5px',
+              margin: '0 auto',
+            }}>
+              <b>$50 STILL AVAILABLE</b>
+            </button>
         </div>
+          </a></div>
       </div>
     </div>
   );
@@ -43,13 +41,13 @@ const ImageGrid: React.FC = () => {
           <img src="/images/rhr/hrf_logo.jpg" alt="Human Rights Foundation" />
         </div>
         <div className="logos">
-          <img src="/images/rhr/strike_logo.jpg" alt="Strike" />
-          <img src="/images/rhr/zeus_logo.jpg" alt="Zeus" />
+        <img src="/images/rhr/strike_logo.jpg" alt="Strike" />
+        <img src="/images/rhr/zeus_logo.jpg" alt="Zeus" />
         </div>
       </div>
       <div className="contact-text">
         <p>Interested in becoming a sponsor?</p>
-        <p>Get in touch: <a href="mailto:events@port8333.io?subject=Hot Style Takeover Sponsorship"><b>events@port8333.io</b></a></p>
+        <p>Get in touch: <a href="mailto:events@port8333.io?subject=Hot Style Takeover Sponsorship">events@port8333.io</a></p>
       </div>
     </div>
   );
@@ -70,28 +68,29 @@ const YourPageWithPriceAndName: React.FC = () => {
           .side-by-side-container {
             display: flex;
             justify-content: center;
-            align-items: flex-start; /* Align items to the top */
+            align-items: center;
             flex: 1 0 auto;
             background-color: black;
-            padding-top: 20px; /* Adjust padding for top spacing */
+            padding-bottom: 20px; /* Add padding bottom for spacing */
           }
 
           .item-container {
             display: flex;
             flex-direction: column;
             align-items: center;
+            padding-bottom: 20px;
             margin: 0 5px;
-            text-align: center; /* Center text */
           }
 
           .item-image {
-            width: 50%; /* Make the image 50% smaller */
+            width: 100%;
           }
 
           .item-name, .item-price {
             color: white;
             margin-top: 5px;
             font-family: sans-serif;
+            text-align: center;
           }
 
           .item-name {
@@ -180,8 +179,11 @@ const YourPageWithPriceAndName: React.FC = () => {
       </style>
       <SideBySideImagesWithPrice 
         imageUrl1="/images/rhr/hotstyle-ga.jpg"
+        imageUrl2="/images/rhr/hotstyle-vip-sodl.jpg"
         linkUrl1="https://pay.zaprite.com/pl_Ss72YiBcvL"
+        linkUrl2="#"
         itemName1="GA HOT STYLE - JULY 25TH"
+        itemName2="VIP HOT STYLE - JULY 25TH"
       />
       <ImageGrid />
     </div>
